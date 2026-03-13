54cadd0dc445e3f359eb4073714ee42d8792f492
Juan <juanalbertoh.fernandez13@gmail.com>
2026-03-13 16:35:45 +0100

    Inicialización del proyecto

---
+++ src/main/java/me/juanalbeticohf/apirest_basica_mysql/ApirestBasicaMysqlApplication.java
@@ -0,0 +1,13 @@
+package me.juanalbeticohf.apirest_basica_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBasicaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestBasicaMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/juanalbeticohf/apirest_basica_mysql/ApirestBasicaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package me.juanalbeticohf.apirest_basica_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestBasicaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
