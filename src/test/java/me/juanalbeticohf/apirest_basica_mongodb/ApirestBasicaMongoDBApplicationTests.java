3f6899c76ae49158c5b2f383beeb990d8986fab9
Juan <juanalbertoh.fernandez13@gmail.com>
2026-03-25 21:50:40 +0100

    fix: cambia el nombre de la clase de los tests para que coincida con la clase principal de SpringBoot

--- src/test/java/me/juanalbeticohf/apirest_basica_mongodb/ApirestBasicaMongoDBApplicationTests.java
+++ src/test/java/me/juanalbeticohf/apirest_basica_mongodb/ApirestBasicaMongoDBApplicationTests.java
@@ -1,10 +1,10 @@
-package me.juanalbeticohf.apirest_basica_mysql;
+package me.juanalbeticohf.apirest_basica_mongodb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApirestBasicaMysqlApplicationTests {
+class ApirestBasicaMongoDBApplicationTests {
 
 	@Test
 	void contextLoads() {
